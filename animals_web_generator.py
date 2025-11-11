56466eca9ad7b653f5b43ab66ff4ea36451acc5a
bastian-westholt <mail@bastianwestholt.com>
2025-11-11 12:25:32 +0100

    Third commit, created html with extracted data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,18 +1,43 @@
 import json
+import os
 
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-def print_data(animals_data):
-    for animal in animals_data:
-        print(f'Name: {animal["name"]}')
-        print(f'Diet: {animal["characteristics"]["diet"]}')
-        print(f'Location: {animal["locations"][0]}')
-        if "type" in animal["characteristics"]:
-            print(f'Type: {animal["characteristics"]["type"]}')
-        print()
 
-animals_data = load_data('animals_data.json')
-print_data(animals_data)
+def get_data(animals_data):
+    animals_data_str = ''
+    for animal in animals_data:
+        animals_data_str += f'Name: {animal["name"]}\n'
+        animals_data_str += f'Diet: {animal["characteristics"]["diet"]}\n'
+        animals_data_str += f'Location: {animal["locations"][0]}\n'
+        if "type" in animal["characteristics"]:
+            animals_data_str += f'Type: {animal["characteristics"]["type"]}\n'
+        animals_data_str += '\n'
+    return animals_data_str
+
+
+def replace_html_content(template_path, output_path, animals_data_str):
+    with open(template_path, "r") as reader:
+        html_content = reader.readlines()
+
+    new_html_content = []
+    for line in html_content:
+        if '__REPLACE_ANIMALS_INFO__' in line:
+            line = line.replace('__REPLACE_ANIMALS_INFO__', animals_data_str)
+        new_html_content.append(line)
+
+    with open(output_path, "w") as writer:
+        writer.writelines(new_html_content)
+
+
+def main():
+    animals_data = load_data('animals_data.json')
+    animals_data_str = get_data(animals_data)
+    replace_html_content('animals_template.html', 'animals.html', animals_data_str)
+
+
+if __name__ == '__main__':
+    main()
