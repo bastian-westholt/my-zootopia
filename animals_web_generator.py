c7986739e85a864c1dc0c44cc84b7e58b0bb111e
bastian-westholt <mail@bastianwestholt.com>
2025-11-11 12:50:39 +0100

    Fifth commit, improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,12 +11,14 @@ def get_data(animals_data):
     animals_data_str = ''
     for animal in animals_data:
         animals_data_str += f'<li class="cards__item">\n'
-        animals_data_str += f'Name: {animal["name"]}<br/>\n'
-        animals_data_str += f'Diet: {animal["characteristics"]["diet"]}<br/>\n'
-        animals_data_str += f'Location: {animal["locations"][0]}<br/>\n'
+        animals_data_str += f'  <div class="card__title">Name: {animal["name"]}</div>\n'
+        animals_data_str += f'  <p class="card__text">\n'
+        animals_data_str += f'      <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+        animals_data_str += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
         if "type" in animal["characteristics"]:
-            animals_data_str += f'Type: {animal["characteristics"]["type"]}<br/>\n'
-        animals_data_str += f'</li>'
+            animals_data_str += f'      <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+        animals_data_str += f'  </p>\n'
+        animals_data_str += f'</li>\n'
         animals_data_str += '\n'
     return animals_data_str
 
@@ -28,7 +30,7 @@ def replace_html_content(template_path, output_path, animals_data_str):
     new_html_content = []
     for line in html_content:
         if '__REPLACE_ANIMALS_INFO__' in line:
-            line = line.replace('__REPLACE_ANIMALS_INFO__', animals_data_str)
+            line = line.strip().replace('__REPLACE_ANIMALS_INFO__', animals_data_str)
         new_html_content.append(line)
 
     with open(output_path, "w") as writer:
@@ -38,7 +40,7 @@ def replace_html_content(template_path, output_path, animals_data_str):
 def main():
     animals_data = load_data('animals_data.json')
     animals_data_str = get_data(animals_data)
-    print(animals_data_str)
+    #print(animals_data_str)
     replace_html_content('animals_template.html', 'animals.html', animals_data_str)
 
 
